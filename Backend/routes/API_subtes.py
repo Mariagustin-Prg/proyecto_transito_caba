ab19a09743f0b6d0eaf798335ca1e177e88daf88
Mariagustin-Prg <mariagustin.acosta1703@gmail.com>
2025-01-20 19:44:12 -0300

    feat[Backend][routes] add API_subtes route

---
+++ Backend/routes/API_subtes.py
@@ -0,0 +1,16 @@
+from fastapi import APIRouter, HTTPException, status
+from Backend.schemas.db_client import clientDB
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Crear una instancia de APIRouter
+router = APIRouter(prefix="/subtes",
+                    tags="API-Subtes"
+                    )
+
+@router.get("/")
+def main():
+    return {
+        "message": "Welcome to the Subtes API"
+    }
