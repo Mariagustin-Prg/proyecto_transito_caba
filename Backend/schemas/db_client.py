3baf40a353223d9ba9376d651507ab990f626a06
Mariagustin-Prg <mariagustin.acosta1703@gmail.com>
2025-01-24 17:38:08 -0300

    fix[Backend][schemas] fix connection with mongo cluster

--- Backend/schemas/db_client.py
+++ Backend/schemas/db_client.py
@@ -7,12 +7,16 @@ load_dotenv(dotenv_path= "./config/.env", encoding="UTF-8")
 DB_USER = os.getenv("DB_USER")
 DB_PWORD = os.getenv("DB_PWORD")
 
-# Connection string
-connection_string = f"mongodb://{DB_USER}:{DB_PWORD}@transito-caba"
 
-# Create a MongoClient
-clientDB = MongoClient(connection_string)
+uri = f"mongodb+srv://mariagustinprog:{DB_PWORD}@transito-caba.gpoy4.mongodb.net/?retryWrites=true&w=majority&appName=Transito-Caba"
 
-# Access a specific database
-db = clientDB.get_database('transito_caba')
+# Create a new client and connect to the server
+client = MongoClient(uri)
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
 
